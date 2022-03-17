cc0a031c324c8585b07126805f4933393c8c7692
ST235 <ST235@ST235-PC01>
2022-03-17 11:06:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace basicMath
+{
+    public class Cal
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            return num1 /num2;
+        }
+
+
+        static void Main (string [] args)
+        {
+            double m;
+            Cal a = new Cal();
+            m = a.Add(2.5, 5.3);
+
+            Console.WriteLine(m);
+        }
+    }
+}
